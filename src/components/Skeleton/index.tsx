f068c6091797019e52a4f1822b02f92c7fc16ce7
ascension-group <ascensionprotocol.dev@gmail.com>
2022-01-18 10:36:44 -0500

    updated

--- src/components/Skeleton/index.tsx
+++ src/components/Skeleton/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Skeleton(): JSX.Element {
     return (
-        <div className="w-24 h-4 rounded-sm bg-gradient-to-r from-gray-400 to-gray-300 animate-pulse "></div>
+        <div className="w-24 h-5 rounded-sm bg-gray-400 animate-pulse "></div>
     );
 }
--- src/hooks/useDistributor.ts
+++ src/hooks/useDistributor.ts
@@ -3,14 +3,4 @@ import { useContract } from "./useContract";
 import contractsInfo from "../constants/contractsInfo.json";
 import { useWeb3React } from "@web3-react/core";
 
-export function useDistributor() {
-    const { account, library } = useWeb3React();
-    const distributor = useContract(
-        contractsInfo.contracts.AscensionDiamond.address,
-        contractsInfo.contracts.ERC20DistributorFacet.abi
-    );
-
-    async function withdrawAll() {}
-
-    return {};
-}
+export function useDistributor() {}
