082c8a7bb73d74e928d99a80e0914c6c7b2b72ae
ascension-group <ascensionprotocol.dev@gmail.com>
2022-04-06 14:34:50 -0400

    meta changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,25 +26,17 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <title key="title">Ascension Protocol</title>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
-
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5" />
         <meta key="description" name="description" content="Web application for Ascension Protocol" />
-
         <meta name="application-name" content="Ascension App" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Ascension App" />
-
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/browserconfig.xml" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#F338C3" />
-
         <meta key="twitter:card" name="twitter:card" content="app" />
         <meta key="twitter:title" name="twitter:title" content="Ascension App" />
         <meta key="twitter:url" name="twitter:url" content="https://app.ascensionprotocol.io" />
@@ -52,9 +44,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <meta key="twitter:image" name="twitter:image" content="" />
         <meta key="twitter:creator" name="twitter:creator" content="@AscendProtocol" />
         <meta key="og:type" property="og:type" content="website" />
-        <meta key="og:site_name" property="og:site_name" content="Ascension App" />
+        <meta key="og:site_name" property="og:site_name" content="Ascension Protocol web application" />
         <meta key="og:url" property="og:url" content="https://app.ascensionprotocol.io" />
-        <meta key="og:image" property="og:image" content="/images/OG.png" />
+        <meta key="og:image" property="og:image" content="/images/ASCEND-logo-vertical.svg" />
         <meta key="og:description" property="og:description" content="Web application for Ascension Protocol" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
