5f6d6763e72e809376d825f6ad101d13ea600570
ascension-group <ascensionprotocol.dev@gmail.com>
2022-05-20 11:41:26 -0400

    remove next image

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useEthers } from '@usedapp/core'
 import makeBlockie from 'ethereum-blockies-base64'
-import Image from 'next/image'
+
 import { BURN_ADDRESS } from '../../constants'
 export default function Avatar({ size = 64 }) {
   const { account } = useEthers()
 
   return (
-    <Image
+    <img
       layout="fixed"
       src={makeBlockie(account ?? BURN_ADDRESS)}
       alt="Blockie"
