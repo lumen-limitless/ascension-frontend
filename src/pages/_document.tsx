c3bb590909b02ddd517788c12a061e678780ec40
ascension-group <ascensionprotocol.dev@gmail.com>
2022-06-14 19:10:05 -0500

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,10 +7,6 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"
-        />
         <meta key="description" name="description" content={APP_DESCRIPTION} />
         <meta name="application-name" content={APP_NAME} />
         <meta name="apple-mobile-web-app-capable" content="yes" />
