46cfcc731d86f639c3ee1667fb55d25cb495d7fa
ascension-group <ascensionprotocol.dev@gmail.com>
2022-06-17 14:09:04 -0400

    update next config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,12 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   trailingSlash: true,
+  il8n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
   images: {
+    formats: ['image/webp'],
     loader: 'custom',
     domains: ['res.cloudinary.com'],
   },
