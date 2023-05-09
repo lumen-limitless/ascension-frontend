81f3a0ac65042f09da2a80fb1aef613422ec7963
Anonymous <>
2023-05-09 15:04:30 -0400

    page titles

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,4 +1,9 @@
 import DashboardPage from './dashboard-page'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
 
 export default function Dashboard() {
   return <DashboardPage />
--- src/app/(app)/stake/page.tsx
+++ src/app/(app)/stake/page.tsx
@@ -1,4 +1,9 @@
 import StakePage from './stake-page'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Stake',
+}
 
 export default function Stake() {
   return <StakePage />
